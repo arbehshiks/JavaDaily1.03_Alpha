5b998218a81a10a88198b8f87f9481f1391c8ae6
Daniel <riwlay@gmail.com>
2019-05-16 19:46:41 +0300

    Profile picture(not ready)

--- app/src/main/java/com/example/javadaily/Activities/Profile.java
+++ app/src/main/java/com/example/javadaily/Activities/Profile.java
@@ -1,21 +1,40 @@
 package com.example.javadaily.Activities;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.ImageView;
 
 import com.example.javadaily.R;
+
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.support.v4.provider.FontsContractCompat.FontRequestCallback.RESULT_OK;
 
 public class Profile extends Fragment {
 
     @Nullable
-    ImageButton blankProfilePicture;
+    ImageButton ProfilePicBtn;
+    private CircleImageView ProfilePic;
+    private static final int PICK_IMAGE = 1;
+    Uri imageUri;
+    ImageView ProfilePicture;
+    static int PReqCode = 1;
+    static int REQUESCODE = 1;
+    private Context context;
+
 
     @Override
 
@@ -25,16 +44,51 @@ public class Profile extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        blankProfilePicture = (ImageButton) rootView.findViewById(R.id.blankProfilePicture);
-        blankProfilePicture.setOnClickListener(new View.OnClickListener() {
+
+
+        CircleImageView ProfilePic = (CircleImageView) rootView.findViewById(R.id.profilePic);
+        ProfilePic.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                System.out.println("Change profile picture");
+            public void onClick(View view) {
+
+                    openGallery();
+
             }
         });
 
         return rootView;
 
     }
+
+
+
+    private void openGallery(){
+        //TODO: open gallery intent and wait for user to pick an image!
+
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(galleryIntent, "Select Picture"), PICK_IMAGE);
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK){
+            imageUri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), imageUri);
+                ProfilePic.setImageBitmap(bitmap);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+
 
 }
